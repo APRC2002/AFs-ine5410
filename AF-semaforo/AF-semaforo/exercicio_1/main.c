6bd3766cb6adbe46317b2d090865e7a3d7a6377a
phpkannenberg <pedrokannenberg@hotmail.com>
2025-10-15 14:04:57 -0300

    Exclusão da struct

--- AF-semaforo/AF-semaforo/exercicio_1/main.c
+++ AF-semaforo/AF-semaforo/exercicio_1/main.c
@@ -6,37 +6,29 @@
 
 FILE* out;
 
-sem_t sem_A;
-sem_t sem_B;
-
-typedef struct {
-    sem_t* meu_semaforo;
-    sem_t* outro_semaforo;
-    int iters;
-} thread_args;
+sem_t sem_A, sem_B;
 
 void *thread_a(void *args) {
-    thread_args *arg = (thread_args *) args;
-    sem_t* sem_A = arg->meu_semaforo;
-    sem_t* sem_B = arg->outro_semaforo;
-    for (int i = 0; i < arg->iters; ++i) {
-        sem_wait(sem_A);
+    for (int i = 0; i < *(int*)args; ++i) {
+	//      +---> arquivo (FILE*) destino
+	//      |    +---> string a ser impressa
+	//      v    v
+	sem_wait(&sem_A);
         fprintf(out, "A");
+        // Importante para que vocês vejam o progresso do programa
+        // mesmo que o programa trave em um sem_wait().
         fflush(stdout);
-        sem_post(sem_B);
+        sem_post(&sem_B);
     }
     return NULL;
 }
 
 void *thread_b(void *args) {
-    thread_args *arg = (thread_args *) args;
-    sem_t* sem_B = arg->meu_semaforo;
-    sem_t* sem_A = arg->outro_semaforo;
-    for (int i = 0; i < arg->iters; ++i) {
-        sem_wait(sem_B);
+    for (int i = 0; i < *(int*)args; ++i) {
+        sem_wait(&sem_B);
         fprintf(out, "B");
         fflush(stdout);
-        sem_post(sem_A);
+        sem_post(&sem_A);
     }
     return NULL;
 }
@@ -56,25 +48,14 @@ int main(int argc, char** argv) {
     sem_init(&sem_A, 0, 1);
     sem_init(&sem_B, 0, 1);
 
-    // Cria estrutura de argumentos para as threads
-    thread_args arg_A;
-    arg_A.meu_semaforo = &sem_A;
-    arg_A.outro_semaforo = &sem_B;
-    arg_A.iters = iters;
-
-    thread_args arg_B;
-    arg_B.meu_semaforo = &sem_B;
-    arg_B.outro_semaforo = &sem_A;
-    arg_B.iters = iters;
-
     // Cria threads
-    pthread_create(&ta, NULL, thread_a, (void *) &arg_A);
-    pthread_create(&tb, NULL, thread_b, (void *) &arg_B);
+    pthread_create(&ta, NULL, thread_a, &iters);
+    pthread_create(&tb, NULL, thread_b, &iters);
 
     // Espera pelas threads
     pthread_join(ta, NULL);
     pthread_join(tb, NULL);
-
+    
     // Destrói semáforos
     sem_destroy(&sem_A);
     sem_destroy(&sem_B);
